ee071a592bf8fb6e7397bbee3cf397c961033c72
Alexander Pajer <alexander.pajer@integrations.at>
2019-12-19 08:24:49 +0100

    L5 boilerplate

---
+++ Lab/Food/L5 FoodListPublish/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  authEnabled: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAllsmrPlc8pTFLQyBOoPfDFj11Gy1PjNA",
+    authDomain: "ngdemoappx.firebaseapp.com",
+    databaseURL: "https://ngdemoappx.firebaseio.com",
+    projectId: "ngdemoappx",
+    storageBucket: "",
+    messagingSenderId: "959870775067",
+    appId: "1:959870775067:web:5c9181bb687daf877f6dbe"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
