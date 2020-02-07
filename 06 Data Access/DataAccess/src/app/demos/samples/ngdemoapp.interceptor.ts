60c2ab60303b0c1afb55f4582e25bfe8e26f9470
Alexander Pajer <alexander.pajer@integrations.at>
2020-02-07 19:18:29 +0100

    ng update

--- 06 Data Access/DataAccess/src/app/demos/samples/ngdemoapp.interceptor.ts
+++ 06 Data Access/DataAccess/src/app/demos/samples/ngdemoapp.interceptor.ts
@@ -6,7 +6,9 @@ import {
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { Injectable } from "@angular/core";
 
+@Injectable()
 export class NgDemoAppInterceptor implements HttpInterceptor {
   public intercept(
     req: HttpRequest<any>,
